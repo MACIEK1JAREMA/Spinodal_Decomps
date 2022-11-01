837db4f1deb5b54913b6d8732a05e1d9fb7c8ea0
MACIEK1JAREMA <87180511+MACIEK1JAREMA@users.noreply.github.com>
2022-11-01 11:10:16 +0000

    First Anisotropic model attempts

--- Ising MC/MC_module.py
+++ Ising MC/MC_module.py
@@ -12,7 +12,7 @@ def MC(N, J, T, t0, tm, nth=1):
     Params:
       ---------------------------
      -- N - int64 - size of lattice side
-     -- J - float64 - sisotropic coupling constant
+     -- J - float64 - isotropic coupling constant
      -- T - float64 - temperature, remembering that Tc = 2.2692*J
      -- t0 - int64 - MCS to start saving from to exclude init conditions
      -- tm - int64 - max MCS
@@ -72,6 +72,81 @@ def MC(N, J, T, t0, tm, nth=1):
     return configs
 
 
+@jit(nopython=True)
+def MC_ani(N, Jx, Jy, T, t0, tm, nth=1, seed=None):
+    """
+    Function to compute Monte Carlo simulation of 2D Ising Model
+    
+    Params:
+      ---------------------------
+     -- N - int64 - size of lattice side
+     -- Jx - float64 - x direction coupling strength
+     -- Jy - float64 - y direction coupling strength
+     -- T - float64 - temperature, remembering that Tc = 2.2692*J
+     -- t0 - int64 - MCS to start saving from to exclude init conditions
+     -- tm - int64 - max MCS
+     -- nth - int64 - optional - every nth MCS to save
+     -- seed - int64 - optional - seed for random initial config.
+    
+    Returns:
+        ----------------------
+    -- configs - np.ndarray - size [N x N x ((t0-tm)/nth)] for lattice at each
+                            desired MSC. Includes intiial random set up at
+                            step 0, so array is 1 larger than expected from t0
+                            tm and nth. The lattice as is after MCS number 4
+                            is saved in column with index 4
+    """
+    
+    if seed is not None:
+        np.random.seed(seed)
+    
+    # generate lattice
+    lat = np.random.rand(N, N)
+    lat = np.sign(lat - 0.5)
+    
+    # number of MCS to complete:
+    num = int(np.floor((tm-t0)/nth)) + 1
+    
+    # generate random comparison numbers
+    z = np.random.rand(1, tm*N**2)  # random number for attempt of each MCS
+    
+    # set up nn index arrays:
+    inds = np.arange(0, N, 1)
+    inds_c = np.ones((N,1), dtype=np.int64)*inds
+    inds_r = inds_c.T
+    
+    nn_t = np.roll(inds_r, N)
+    nn_b = np.roll(inds_r, -N)
+    nn_r = nn_b.T
+    nn_l = nn_t.T
+    
+    # save results:
+    #configs = np.zeros((N, N, tm))
+    configs = np.zeros((N, N, num+1))
+    configs[:, :, 0] = lat
+    
+    # plot the resulting image onto that plot
+    
+    for t in range(1, tm+1):
+        for n in range(N**2):
+            # pick a random spin site:
+            i = np.random.randint(N)
+            j = np.random.randint(N)
+            # calculate energy factor:
+            dEx = 2*Jx*lat[i, j]*( lat[i, nn_r[i, j]] + lat[i, nn_l[i, j]] )
+            dEy = 2*Jy*lat[i, j]*( lat[nn_t[i, j], j] + lat[nn_b[i, j], j] )
+            dE = dEx + dEy
+            r = np.exp(-dE/T)
+            if r > z[0, (t-t0)*n + n]:
+                lat[i, j] *= -1
+            # otherwise do nothing
+        # save configuration from this MCS:
+        if t >= t0 and (t-t0) % nth == 0:
+            configs[:, :, int((t-t0)/nth)+1] = lat
+    
+    return configs
+
+
 def annulus_avg(ft, N, dk):
     """
     Finds average of 
@@ -122,6 +197,7 @@ def Sk_MCrun(N, J, T, dk, t0, tm, nth):
     
     return average
 
+
 # %%
 
 if __name__ == "__main__":
@@ -132,7 +208,7 @@ if __name__ == "__main__":
     J = 1
     Tc = 2.2692*J
     T = 0.1*Tc
-    t0 = 1
+    t0 = 1  # at least 1 (0th saved automatically anyway!)
     tm = 5
     
     fig = plt.figure(figsize=(10, 7))
@@ -156,3 +232,36 @@ if __name__ == "__main__":
         print('showing '+ str(t))
         ax.imshow(configs[:, :, t], cmap='Greys', vmin=-1, vmax=1, interpolation=None)
         plt.pause(0.5)
+
+# %%
+        
+# anisotropic code:
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    # set up lattice and variables
+    N = 256
+    
+    Jx = 10
+    Jy = 0.1
+#    Jx = 1
+#    Jy = 1
+    Tc = 2.2692*np.sqrt(Jx**2 + Jy**2)  # just as zeroth order approx here.
+    T = 0.01*Tc
+    t0 = 1
+    tm = 150
+    
+    fig = plt.figure(figsize=(10, 7))
+    ax = fig.gca()
+    ax.tick_params(labelsize=16)
+    ax.set_xticks(np.arange(0, N+1, int(N/5)))
+    ax.set_yticks(np.arange(0, N+1, int(N/5)))
+    
+    nth = 5
+    configs = MC_ani(N, Jx, Jy, T, t0, tm, nth=nth, seed=1314)
+    
+    # animate it:
+    for t in range(len(configs[0, 0, :])):
+        ax.imshow(configs[:, :, t], cmap='Greys', vmin=-1, vmax=1, interpolation=None)
+        plt.pause(0.01)
+
