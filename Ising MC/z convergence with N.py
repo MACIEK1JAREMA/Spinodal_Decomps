a5a2c2e411185363ba394ca68d15012d3da5800d
MACIEK1JAREMA <87180511+MACIEK1JAREMA@users.noreply.github.com>
2022-11-04 13:37:37 +0000

    Code to test z convergence with N

---
+++ Ising MC/z convergence with N.py
@@ -0,0 +1,90 @@
+import numpy as np
+import MC_module as MC
+import matplotlib.pyplot as plt
+from matplotlib import ticker as mtick
+from scipy.stats import linregress as linreg
+import timeit
+
+# %%
+
+# start the timer
+start = timeit.default_timer()
+
+# set up lattice and variables
+J = 1
+Tc = 2.2692*J
+T = 0.1*Tc
+
+moment = 1
+
+reps = 10  # number of runs over different initial conditions
+dk = 1
+n_nths = 12
+
+#Ns = 2**np.arange(5, 10, 1, dtype=np.int64)
+Ns = np.arange(30, 500, 50, dtype=np.int64)
+
+exponents = np.zeros(len(Ns))
+
+for j in range(len(Ns)):
+    t0 = int(Ns[j]/10)
+    tm = int(Ns[j]*0.8)
+    nth = int((tm-t0)/12)
+    
+    # set up arrays and length values:
+    mcss = int(np.floor((tm-t0)/nth)) + 2
+    k_num = len(np.arange(1, int(Ns[j]/2), dk))
+    
+    # find circularly averages S(kx, ky) = S(k) for each MCS of each initial
+    # realisation and store in 3D array
+    average = np.zeros((k_num, mcss, reps))
+    for i in range(reps):
+        average[:, :, i], kvals = MC.Sk_MCrun(Ns[j], J, T, dk, t0, tm, nth=nth)
+        print("Finished repetition " + str(i) + " of N value = " + str(Ns[j]))
+    
+    avgSk = np.sum(average, axis=2)/reps
+    avgSk_norm = avgSk / avgSk[:, 0][:, None]
+    
+    kvals = (2*np.pi/Ns[j])*np.arange(1, int(Ns[j]/2), dk)
+    
+    k_vals = np.tile(kvals, (len(avgSk_norm[0, :]), 1)).T
+    k = np.sum(avgSk_norm*k_vals**(moment+1)*dk, axis=0)/np.sum(avgSk_norm*k_vals*dk, axis=0)
+    L = (2*np.pi/k)**(1/moment)
+    
+    t_vals = nth*(np.arange(1, len(avgSk_norm[0, :]), 1) - 1) + t0
+    
+    # check the gradient of the linear ones
+    exponents[j] = linreg(np.log(t_vals), np.log(L[1:]))[0]
+    
+
+# plot it as a function of t on log log:
+fig = plt.figure(figsize=(10, 7))
+ax = fig.gca()
+ax.set_xlabel(r'$N$', fontsize=22)
+ax.set_ylabel(r'$1/z$', fontsize=22)
+ax.plot(Ns, exponents)
+
+# return time to run
+stop = timeit.default_timer()
+print('Time: ', stop - start)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
