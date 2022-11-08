9f58ae20bb1acbe647c7ca065fa2d0dd0dd26a29
MACIEK1JAREMA <87180511+MACIEK1JAREMA@users.noreply.github.com>
2022-11-08 10:26:11 +0000

    More code for combined figures
    Deleted test data too

--- Codes for combined figures/L vs t combined.py
+++ Codes for combined figures/L vs t combined.py
@@ -10,28 +10,37 @@ import pandas as pd
 
 # %%
 
-# Read in both data sets for Sk averaged over many initial considtions
+# Read in MC data for Sk averaged over many initial considtions
 # Need directory on Codes for combined figures
 Sk1 = pd.read_excel("Data\MC\Sk_avg_over_reps.xlsx", index_col=0)
 Sk1 = Sk1.to_numpy()
+t_vals1 = pd.read_excel("Data\MC\Sk_avg_over_reps_t_vals.xlsx", index_col=0)
+t_vals1 = t_vals1.to_numpy()[:, 0]
+kvals1 = pd.read_excel("Data\MC\Sk_avg_over_reps_k_vals.xlsx", index_col=0)
+kvals1 = kvals1.to_numpy()[:, 0]
 
-t_vals = pd.read_excel("Data\MC\Sk_avg_over_reps_t_vals.xlsx", index_col=0)
-t_vals = t_vals.to_numpy()[:, 0]
+# Read in GLT data
+num_time_steps = 16
+averaged_sf2 = np.zeros(num_time_steps, dtype=object)
+for i in range(num_time_steps):
+    name = "Data\GLT\model A average unnormalised sf #"+str(i)+" over 10 inits.txt)"
+    averaged_sf2[i] = np.loadtxt(name)
+kvals2 = np.loadtxt("Data\GLT\model A kvals.txt")
+time_steps2 = np.loadtxt("Data\GLT\model A time steps.txt")
 
-kvals = pd.read_excel("Data\MC\Sk_avg_over_reps_k_vals.xlsx", index_col=0)
-kvals = kvals.to_numpy()[:, 0]
-
-dk = 1
+# normalise each
+for sf in averaged_sf2[1:]:
+    normalised_sf2 = sf/averaged_sf2[0]
 
 # Normalise to intial
-avgSk_norm = Sk1 / Sk1[:, 0][:, None]
+avgSk_norm1 = Sk1 / Sk1[:, 0][:, None]
 
 # find average k
 moment = 1
-
-k_vals_arr = np.tile(kvals, (len(avgSk_norm[0, :]), 1)).T
-k = np.sum(avgSk_norm*k_vals_arr**(moment+1)*dk, axis=0)/np.sum(avgSk_norm*k_vals_arr*dk, axis=0)
-L = (2*np.pi/k)**(1/moment)  # get L
+dk = 1   # from when data was run
+k_vals_arr1 = np.tile(kvals1, (len(avgSk_norm1[0, :]), 1)).T
+k1 = np.sum(avgSk_norm1*k_vals_arr1**(moment+1)*dk, axis=0)/np.sum(avgSk_norm1*k_vals_arr1*dk, axis=0)
+L1 = (2*np.pi/k1)**(1/moment)  # get L
 
 # plot resulting S(k) at each t step
 figSn = plt.figure(figsize=(10, 7))
@@ -39,11 +48,11 @@ axSn = figSn.gca()
 axSn.tick_params(labelsize=22)
 axSn.set_xlabel(r"$k$", fontsize=22)
 axSn.set_ylabel(r"S($k$)$/$S($k$)$|_{t=0}$", fontsize=22)
-for i in range(1, len(avgSk_norm[0, :])):
+plotted_ts = [1, 5, 10, 15]   # Update once data is produced
+for i in plotted_ts:   # Update once data is produced
     #time = str(int(nth*(i-1) + t0)) + " MCS"
-    time = str(int(t_vals[i-1])) + " MCS"
-    axSn.plot(kvals, avgSk_norm[:, i], label=r"$t=$"+time)
-axSn.legend(fontsize=22)
+    time = str(int(t_vals1[i])) + " MCS"
+    axSn.plot(kvals1, avgSk_norm1[:, i], label=r"$t=$"+time)
 
 # plot L vs t on log log:
 fig = plt.figure(figsize=(10, 7))
@@ -54,14 +63,14 @@ ax.set_ylabel(r'$L(t)$', fontsize=22)
 ax.set_yscale('log')
 ax.set_xscale('log')
 ax.tick_params(labelsize=22)
-ax.plot(t_vals, L[1:])  # omit initial condition as at t=0
+ax.plot(t_vals1, L1[1:], 'g^', ms=10)  # omit initial condition as at t=0
 
 # check the gradient of the linear ones
-m1, c1, rval1, _, std1 = linreg(np.log(t_vals), np.log(L[1:]))
+m1, c1, rval1, _, std1 = linreg(np.log(t_vals1), np.log(L1[1:]))
 print(f'Linear variation with gradient = {np.round(m1, 4)} and error +- {np.round(std1, 4)}')
 print(f'with R-value of {np.round(rval1, 4)}')
 print('\n')
-ax.plot(t_vals, np.exp(c1)*t_vals**m1, '-.b', label=f'fit at gradient={np.round(m1, 4)}')
+ax.plot(t_vals1, np.exp(c1)*t_vals1**m1, '-.b', label=f'fit at gradient={np.round(m1, 4)}')
 
 # plot S(k) rescaled for proposed universal scaling relation
 figUni = plt.figure(figsize=(10, 7))
@@ -70,7 +79,14 @@ axUni.tick_params(labelsize=22)
 axUni.set_xlabel(r"$kt^{\frac{1}{z}}$", fontsize=22)
 axUni.set_ylabel(r"$\frac{S(k) t^{-2/z} }{S(k)|_{t=0}}$", fontsize=22)
 
-for i in range(1, len(avgSk_norm[0, :])):
+for i in plotted_ts:
     #time = str(int(nth*(i-1) + t0)) + " MCS"
-    time = str(int(t_vals[i-1])) + " MCS"
-    axUni.plot(kvals*t_vals[i-1]**m1, avgSk_norm[:, i]/t_vals[i-1]**(2*m1), label=r"$t=$"+time)
+    time = str(int(t_vals1[i-1])) + " MCS"
+    axUni.plot(kvals1*t_vals1[i-1]**m1, avgSk_norm1[:, i]/t_vals1[i-1]**(2*m1), label=r"$t=$"+time)
+
+
+axSn.legend(fontsize=22)
+ax.legend(fontsize=22)
+axUni.legend(fontsize=22)
+
+
--- Codes for combined figures/convergence.py
+++ Codes for combined figures/convergence.py
@@ -11,17 +11,19 @@ import pandas as pd
 # %%
 
 
-# Read in both data sets for 1/z from each N
+# Read in MC data sets for 1/z from each N
 # Need directory on Codes for combined figures
 Ns = pd.read_excel("Data\MC\convergence_N_vals.xlsx", index_col=0)
 Ns = Ns.to_numpy()[:, 0]
-
 exponents = pd.read_excel("Data\MC\convergence_exponents.xlsx", index_col=0)
 exponents = exponents.to_numpy()[:, 0]
-
 exp_errs = pd.read_excel("Data\MC\convergence_exp_errs.xlsx", index_col=0)
 exp_errs = exp_errs.to_numpy()[:, 0]
 
+# Read in GLT data set
+zlist2 = np.loadtxt("Data\GLT\model A 1/z values.txt")
+zerr2 = np.loadtxt("Data\GLT\model A 1/z error bars.txt")
+Ns2 = np.loadtxt("Data\GLT\model A 1/z system sizes.txt")
 
 # plot it as a function of t
 fig = plt.figure(figsize=(10, 7))
@@ -29,5 +31,6 @@ ax = fig.gca()
 ax.tick_params(labelsize=22)
 ax.set_xlabel(r'$N$', fontsize=22)
 ax.set_ylabel(r'$1/z$', fontsize=22)
-ax.errorbar(Ns, exponents, yerr=exp_errs, capsize=2)
-
+ax.errorbar(Ns, exponents, yerr=exp_errs, capsize=2, marker="^", color="g", ms=10, label="MC")
+ax.errorbar(Ns2, zlist2, yerr=zerr2, capsize=2, marker="o", color="r", ms=10, label="GLT")
+ax.legend(fontsize=22)
